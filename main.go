b5092c8a3a4b9c5f2affc1cb7f8252ed479f8188
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-04 12:17:33 +0900

    added: processig to run load balancer

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"flag"
+
+	"github.com/hlts2/go-LB/config"
+	"github.com/hlts2/go-LB/server"
+	"github.com/kpango/glg"
 )
 
 var (
-	cofingFileN string
+	configFileN string
+	addr        string
+	port        string
 )
 
 func init() {
-	flag.StringVar(&cofingFileN, "s", "config.yaml", "set a config file of load balancer")
+	flag.StringVar(&configFileN, "s", "config.yaml", "set a config file of load balancer")
+	flag.StringVar(&addr, "a", "0.0.0.0", "set a host address of load balancer")
+	flag.StringVar(&port, "p", "80", "set a port number of load balancer")
 	flag.Parse()
 }
 
 func main() {
+	conf, err := config.LoadConfig(configFileN)
+	if err != nil {
+		glg.Fatalln(err)
+	}
+
+	lb := server.NewLB(addr + ":" + port)
+
+	err = lb.Build(*conf).Serve()
+	if err != nil {
+		glg.Fatalln(err)
+	}
 }
