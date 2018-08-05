834854d9664ec0105cbe0f8895f77fc42105b2f1
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-05 21:15:59 +0900

    fixed: processing to stat TLS server

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
+	"path/filepath"
 
 	"github.com/hlts2/go-LB/config"
 	"github.com/hlts2/go-LB/server"
@@ -12,12 +14,14 @@ var (
 	configFileN string
 	host        string
 	port        string
+	tlspath     string
 )
 
 func init() {
 	flag.StringVar(&configFileN, "s", "config.yaml", "set a config file of load balancer")
 	flag.StringVar(&host, "h", "127.0.0.1", "set a host name or IP address of load balancer")
 	flag.StringVar(&port, "p", "8080", "set a port number of load balancer")
+	flag.StringVar(&tlspath, "tlf-path", "", "set a TLS directory")
 	flag.Parse()
 }
 
@@ -30,9 +34,28 @@ func main() {
 	}
 
 	lb := server.NewLB(host + ":" + port)
+	lb.Build(conf)
 
-	err = lb.Build(conf).Serve()
-	if err != nil {
-		glg.Fatalln(err)
+	// NOT TLS Mode
+	if tlspath == "" {
+		err = lb.Serve()
+		if err != nil {
+			glg.Fatalln(err)
+		}
+	} else {
+		var (
+			certfile = filepath.Join(tlspath, "server.pem")
+			keyfile  = filepath.Join(tlspath, "key.pen")
+		)
+		cer, err := tls.LoadX509KeyPair(certfile, keyfile)
+		if err != nil {
+			glg.Fatalln(err)
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cer},
+		}
+
+		lb.ServeTLS(tlsConfig, certfile, keyfile)
 	}
 }
