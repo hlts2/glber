c9e371cbee16b420959342f4c2db676eae7ee0ca
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-05 16:19:02 +0900

    fixed: processing to check duplicate hosts

--- config/config.go
+++ config/config.go
@@ -1,10 +1,14 @@
 package config
 
 import (
+	"errors"
 	"os"
 
 	yaml "gopkg.in/yaml.v2"
 )
+
+// ErrDuplicateHost is error that there is duplicte host
+var ErrDuplicateHost = errors.New("duplicate host")
 
 // Config represents config file of load balancer
 type Config struct {
@@ -32,6 +36,18 @@ func (ss Servers) GetAddresses() []string {
 	return hosts
 }
 
+// GetHostWithPort returns host of servers
+// i.e) 192.168.33.10:1111
+func (ss Servers) GetHostWithPort() []string {
+	hosts := make([]string, 0, len(ss))
+
+	for _, s := range ss {
+		hosts = append(hosts, s.Host+":"+s.Port)
+	}
+
+	return hosts
+}
+
 // LoadConfig loads config of load balancer
 func LoadConfig(filename string, conf *Config) error {
 	f, err := os.Open(filename)
@@ -44,5 +60,23 @@ func LoadConfig(filename string, conf *Config) error {
 		return err
 	}
 
+	ok := existsDuplicateHost(conf.Servers.GetHostWithPort())
+	if ok {
+		return ErrDuplicateHost
+	}
+
 	return nil
+}
+
+// existsDuplicateHost returns true if there is duplicte host
+func existsDuplicateHost(hosts []string) bool {
+	m := make(map[string]int, len(hosts))
+
+	for _, host := range hosts {
+		if _, ok := m[host]; ok {
+			return true
+		}
+		m[host] = 0
+	}
+	return false
 }
