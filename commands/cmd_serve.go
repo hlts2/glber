16405461538b452a7311451021c3c43e2b821255
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-06 12:21:35 +0900

    added: cli command and fixed config file name and filed

---
+++ commands/cmd_serve.go
@@ -0,0 +1,83 @@
+package commands
+
+import (
+	"crypto/tls"
+	"path/filepath"
+
+	"github.com/hlts2/go-LB/config"
+	"github.com/hlts2/go-LB/server"
+	"github.com/urfave/cli"
+)
+
+// ServeCommand is the command that serve load balancer
+func ServeCommand() cli.Command {
+	return cli.Command{
+		Name:  "serve",
+		Usage: "serve load balancer",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "set, s",
+				Value: "config.yml",
+				Usage: "set a config file of load balancer",
+			},
+			cli.StringFlag{
+				Name:  "host, H",
+				Value: "127.0.0.1",
+				Usage: "set a host name or IP of load balancer",
+			},
+			cli.StringFlag{
+				Name:  "port, p",
+				Value: "8080",
+				Usage: "set a port number of load balancer",
+			},
+			cli.StringFlag{
+				Name:  "tlspath",
+				Value: "",
+				Usage: "set a TLS directory of load balancer",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			var conf config.Config
+			err := config.LoadConfig(c.String("set"), &conf)
+			if err != nil {
+				return err
+			}
+
+			lb := server.NewLB(c.String("host") + ":" + c.String("port")).Build(conf)
+
+			tlspath := c.String("tlspath")
+
+			// NOT TLS Mode
+			if tlspath == "" {
+				err := lb.Serve()
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+
+			var (
+				certname = filepath.Join(tlspath, "cert.pem")
+				keyname  = filepath.Join(tlspath, "key.pem")
+			)
+
+			cert, err := tls.LoadX509KeyPair(certname, keyname)
+			if err != nil {
+				return err
+			}
+
+			tlsConfig := tls.Config{
+				Certificates: []tls.Certificate{
+					cert,
+				},
+			}
+
+			err = lb.ServeTLS(&tlsConfig, certname, keyname)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+}
--- main.go
+++ main.go
@@ -1,61 +1,24 @@
 package main
 
 import (
-	"crypto/tls"
-	"flag"
-	"path/filepath"
+	"os"
 
-	"github.com/hlts2/go-LB/config"
-	"github.com/hlts2/go-LB/server"
+	"github.com/hlts2/go-LB/commands"
 	"github.com/kpango/glg"
+	"github.com/urfave/cli"
 )
-
-var (
-	configFileN string
-	host        string
-	port        string
-	tlspath     string
-)
-
-func init() {
-	flag.StringVar(&configFileN, "s", "config.yaml", "set a config file of load balancer")
-	flag.StringVar(&host, "h", "127.0.0.1", "set a host name or IP address of load balancer")
-	flag.StringVar(&port, "p", "8080", "set a port number of load balancer")
-	flag.StringVar(&tlspath, "tlf-path", "", "set a TLS directory")
-	flag.Parse()
-}
 
 func main() {
-	var conf config.Config
-
-	err := config.LoadConfig(configFileN, &conf)
-	if err != nil {
-		glg.Fatalln(err)
+	app := cli.NewApp()
+	app.Name = "go-LB"
+	app.Usage = "Load Balancer"
+	app.Version = "v0.0.1"
+	app.Commands = cli.Commands{
+		commands.ServeCommand(),
 	}
 
-	lb := server.NewLB(host + ":" + port)
-	lb.Build(conf)
-
-	// NOT TLS Mode
-	if tlspath == "" {
-		err = lb.Serve()
-		if err != nil {
-			glg.Fatalln(err)
-		}
-	} else {
-		var (
-			certfile = filepath.Join(tlspath, "server.pem")
-			keyfile  = filepath.Join(tlspath, "key.pen")
-		)
-		cer, err := tls.LoadX509KeyPair(certfile, keyfile)
-		if err != nil {
-			glg.Fatalln(err)
-		}
-
-		tlsConfig := &tls.Config{
-			Certificates: []tls.Certificate{cer},
-		}
-
-		lb.ServeTLS(tlsConfig, certfile, keyfile)
+	err := app.Run(os.Args)
+	if err != nil {
+		glg.Fatalln(err)
 	}
 }
