96cbb7f7838be8c3caa8bf45c6deec082e357a7f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-04 12:56:35 +0900

    Add commentout, FIXME: hlts2

--- server/proxy.go
+++ server/proxy.go
@@ -1,48 +1,40 @@
 package server
 
-import (
-	"bytes"
-	"io"
-	"net/http"
-
-	"github.com/kpango/glg"
-)
-
-func (lb *LB) reverseProxy(scheme, destHost string, w http.ResponseWriter, req *http.Request) {
-	req.URL.Scheme = scheme
-	req.URL.Host = destHost
-
-	lb.lf.Wait()
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil {
-		lb.lf.Signal()
-		glg.Println(err)
-		return
-	}
-
-	lb.lf.Signal()
-
-	copyHeader(w, resp)
-
-	w.WriteHeader(resp.StatusCode)
-
-	data := readCloserToByte(resp.Body)
-	w.Write(data)
-
-	resp.Body.Close()
-}
-
-func readCloserToByte(readCloser io.ReadCloser) []byte {
-	buf := new(bytes.Buffer)
-	io.Copy(buf, readCloser)
-	return buf.Bytes()
-}
-
-func copyHeader(dest http.ResponseWriter, src *http.Response) {
-	for key, values := range src.Header {
-		dest.Header().Del(key)
-		for _, value := range values {
-			dest.Header().Add(key, value)
-		}
-	}
-}
+// func (lb *LB) reverseProxy(scheme, destHost string, w http.ResponseWriter, req *http.Request) {
+// 	req.URL.Scheme = scheme
+// 	req.URL.Host = destHost
+//
+// 	lb.lf.Wait()
+// 	resp, err := http.DefaultTransport.RoundTrip(req)
+// 	if err != nil {
+// 		lb.lf.Signal()
+// 		glg.Println(err)
+// 		return
+// 	}
+//
+// 	lb.lf.Signal()
+//
+// 	copyHeader(w, resp)
+//
+// 	w.WriteHeader(resp.StatusCode)
+//
+// 	data := readCloserToByte(resp.Body)
+// 	w.Write(data)
+//
+// 	resp.Body.Close()
+// }
+//
+// func readCloserToByte(readCloser io.ReadCloser) []byte {
+// 	buf := new(bytes.Buffer)
+// 	io.Copy(buf, readCloser)
+// 	return buf.Bytes()
+// }
+//
+// func copyHeader(dest http.ResponseWriter, src *http.Response) {
+// 	for key, values := range src.Header {
+// 		dest.Header().Del(key)
+// 		for _, value := range values {
+// 			dest.Header().Add(key, value)
+// 		}
+// 	}
+// }
--- server/server.go
+++ server/server.go
@@ -1,105 +1,89 @@
 package server
 
-import (
-	"crypto/tls"
-	"net"
-	"net/http"
-
-	"github.com/pkg/errors"
-
-	b "github.com/hlts2/go-LB/balancing"
-	"github.com/hlts2/go-LB/config"
-	iphash "github.com/hlts2/ip-hash"
-	leastconnections "github.com/hlts2/least-connections"
-	lockfree "github.com/hlts2/lock-free"
-	roundrobin "github.com/hlts2/round-robin"
-	"github.com/kpango/glg"
-)
-
-// ErrInvalidBalancingAlgorithm is error of invalid balancing algorithm
-var ErrInvalidBalancingAlgorithm = errors.New("invalid balancing algorithm")
-
-// LB represents load balancer
-type LB struct {
-	*http.Server
-	balancing *b.Balancing
-	lf        lockfree.LockFree
-}
-
-// NewLB returns LB object
-func NewLB(addr string) *LB {
-	return &LB{
-		Server: &http.Server{
-			Addr: addr,
-		},
-		lf: lockfree.New(),
-	}
-}
-
-// Build builds config of load balancer
-func (lb *LB) Build(conf config.Config) *LB {
-	switch conf.Balancing {
-	case "ip-hash":
-		ih, err := iphash.New(conf.Servers.GetAddresses())
-		if err != nil {
-			glg.Fatalln(errors.Wrap(err, "faild to load ip-hash algorithm"))
-		}
-
-		lb.balancing = b.New(ih)
-		lb.Handler = http.HandlerFunc(lb.ipHashBalancing)
-	case "round-robin":
-		rr, err := roundrobin.New(conf.Servers.GetAddresses())
-		if err != nil {
-			glg.Fatalln(errors.Wrap(err, "faild to load round-robin algorithm"))
-		}
-
-		lb.balancing = b.New(rr)
-		lb.Handler = http.HandlerFunc(lb.roundRobinBalancing)
-	case "least-connections":
-		lc, err := leastconnections.New(conf.Servers.GetAddresses())
-		if err == nil {
-			glg.Fatalln(errors.Wrap(err, "faild to load least-connections algorithm"))
-		}
-
-		lb.balancing = b.New(lc)
-		lb.Handler = http.HandlerFunc(lb.ipHashBalancing)
-	default:
-		glg.Fatalln(errors.Wrap(ErrInvalidBalancingAlgorithm, conf.Balancing))
-	}
-
-	return lb
-}
-
-// ServeTLS runs load balancer with TLS
-func (lb *LB) ServeTLS(tlsConfig *tls.Config, certFile, keyFile string) error {
-	lisner, err := net.Listen("tcp", lb.Addr)
-	if err != nil {
-		return errors.Wrap(err, "faild to listen")
-	}
-
-	lb.TLSConfig = tlsConfig
-
-	glg.Success("Load Balancer starting on " + lb.Addr)
-	err = lb.Server.ServeTLS(lisner, certFile, keyFile)
-	if err != nil {
-		return errors.Wrap(err, "faild to serve with tls")
-	}
-
-	return nil
-}
-
-// Serve runs load balancer
-func (lb *LB) Serve() error {
-	lisner, err := net.Listen("tcp", lb.Addr)
-	if err != nil {
-		return errors.Wrap(err, "faild to listen")
-	}
-
-	glg.Success("Load Balancer starting on " + lb.Addr)
-	err = lb.Server.Serve(lisner)
-	if err != nil {
-		return errors.Wrap(err, "faild to serve")
-	}
-
-	return nil
-}
+// // ErrInvalidBalancingAlgorithm is error of invalid balancing algorithm
+// var ErrInvalidBalancingAlgorithm = errors.New("invalid balancing algorithm")
+//
+// // LB represents load balancer
+// type LB struct {
+// 	*http.Server
+// 	// balancing *b.Balancing
+// 	lf lockfree.LockFree
+// }
+//
+// // NewLB returns LB object
+// func NewLB(addr string) *LB {
+// 	return &LB{
+// 		Server: &http.Server{
+// 			Addr: addr,
+// 		},
+// 		lf: lockfree.New(),
+// 	}
+// }
+//
+// // Build builds config of load balancer
+// func (lb *LB) Build(conf config.Config) *LB {
+// 	switch conf.Balancing {
+// 	case "ip-hash":
+// 		ih, err := iphash.New(conf.Servers.GetAddresses())
+// 		if err != nil {
+// 			glg.Fatalln(errors.Wrap(err, "faild to load ip-hash algorithm"))
+// 		}
+//
+// 		// lb.balancing = b.New(ih)
+// 		lb.Handler = http.HandlerFunc(lb.ipHashBalancing)
+// 	case "round-robin":
+// 		rr, err := roundrobin.New(conf.Servers.GetAddresses())
+// 		if err != nil {
+// 			glg.Fatalln(errors.Wrap(err, "faild to load round-robin algorithm"))
+// 		}
+//
+// 		// lb.balancing = b.New(rr)
+// 		lb.Handler = http.HandlerFunc(lb.roundRobinBalancing)
+// 	case "least-connections":
+// 		lc, err := leastconnections.New(conf.Servers.GetAddresses())
+// 		if err == nil {
+// 			glg.Fatalln(errors.Wrap(err, "faild to load least-connections algorithm"))
+// 		}
+//
+// 		// lb.balancing = b.New(lc)
+// 		lb.Handler = http.HandlerFunc(lb.ipHashBalancing)
+// 	default:
+// 		glg.Fatalln(errors.Wrap(ErrInvalidBalancingAlgorithm, conf.Balancing))
+// 	}
+//
+// 	return lb
+// }
+//
+// // ServeTLS runs load balancer with TLS
+// func (lb *LB) ServeTLS(tlsConfig *tls.Config, certFile, keyFile string) error {
+// 	lisner, err := net.Listen("tcp", lb.Addr)
+// 	if err != nil {
+// 		return errors.Wrap(err, "faild to listen")
+// 	}
+//
+// 	lb.TLSConfig = tlsConfig
+//
+// 	glg.Success("Load Balancer starting on " + lb.Addr)
+// 	err = lb.Server.ServeTLS(lisner, certFile, keyFile)
+// 	if err != nil {
+// 		return errors.Wrap(err, "faild to serve with tls")
+// 	}
+//
+// 	return nil
+// }
+//
+// // Serve runs load balancer
+// func (lb *LB) Serve() error {
+// 	lisner, err := net.Listen("tcp", lb.Addr)
+// 	if err != nil {
+// 		return errors.Wrap(err, "faild to listen")
+// 	}
+//
+// 	glg.Success("Load Balancer starting on " + lb.Addr)
+// 	err = lb.Server.Serve(lisner)
+// 	if err != nil {
+// 		return errors.Wrap(err, "faild to serve")
+// 	}
+//
+// 	return nil
+// }
