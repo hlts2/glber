e4c793a682737a181fb7269e31a4ccaf4c3228ec
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-04 19:04:58 +0900

    Deleted unused code

--- pkg/slb/balancer/balancer.go
+++ pkg/slb/balancer/balancer.go
@@ -7,18 +7,3 @@ type Balancer interface {
 	ServeHTTP(http.ResponseWriter, *http.Request)
 	isBalaner()
 }
-
-type balancer struct {
-	next http.Handler
-}
-
-// New --
-func New(next http.Handler) http.Handler {
-	return &balancer{
-		next: next,
-	}
-}
-
-func (b *balancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	b.next.ServeHTTP(w, req)
-}
