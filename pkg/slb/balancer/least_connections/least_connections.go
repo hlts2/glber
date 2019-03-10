882b0fba2701509173389ace117877c6c573de76
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-10 21:12:42 +0900

    Added leastconnection handler

--- pkg/slb/balancer/least_connections/least_connections.go
+++ pkg/slb/balancer/least_connections/least_connections.go
@@ -4,20 +4,34 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/hlts2/least-connections"
+
 	"github.com/hlts2/go-LB/pkg/slb/balancer"
 )
 
-type leastconnections struct {
+type leastconnectionsHandler struct {
+	urls    []*url.URL
+	proxier balancer.Proxier
+	lc      leastconnections.LeastConnections
+
 	balancer.Handler
 }
 
-func (h *leastconnections) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// TODO: not yet implemented
+func (h *leastconnectionsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	src, done := h.lc.Next()
+	defer done()
+	h.proxier.Proxy(src, w, req)
 }
 
-func (h *leastconnections) isBalaner() {}
+func (h *leastconnectionsHandler) isBalaner() {}
 
 // New returns balancer.Handler implementation(*leastconnections).
 func New(urls []*url.URL, proxier balancer.Proxier) balancer.Handler {
-	return new(leastconnections)
+	lc, _ := leastconnections.New(urls)
+
+	return &leastconnectionsHandler{
+		urls:    urls,
+		proxier: proxier,
+		lc:      lc,
+	}
 }
