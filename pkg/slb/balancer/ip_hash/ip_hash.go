31f06aebf2d7d12dcb0f212781a945a7f5621183
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-10 17:45:25 +0900

    Add balancer handler

--- pkg/slb/balancer/ip_hash/ip_hash.go
+++ pkg/slb/balancer/ip_hash/ip_hash.go
@@ -3,11 +3,16 @@ package iphash
 import (
 	"net/http"
 	"net/url"
+	"sync"
 
 	"github.com/hlts2/go-LB/pkg/slb/balancer"
 )
 
 type iphash struct {
+	urls    []*url.URL
+	m       *sync.Map
+	proxier balancer.Proxier
+
 	balancer.Handler
 }
 
@@ -15,9 +20,17 @@ func (h *iphash) ServeHTTP(http.ResponseWriter, *http.Request) {
 	// TODO: not yet implemented
 }
 
+func (h *iphash) pick(url *url.URL) *url.URL {
+	return nil
+}
+
 func (h *iphash) isBalaner() {}
 
 // New returns balancer.Handler implementation(*iphash).
-func New(addrs []url.URL, proxier balancer.Proxier) balancer.Handler {
-	return &iphash{}
+func New(urls []*url.URL, proxier balancer.Proxier) balancer.Handler {
+	return &iphash{
+		urls:    urls,
+		m:       new(sync.Map),
+		proxier: proxier,
+	}
 }
--- pkg/slb/balancer/round_robin/round_robin.go
+++ pkg/slb/balancer/round_robin/round_robin.go
@@ -5,19 +5,27 @@ import (
 	"net/url"
 
 	"github.com/hlts2/go-LB/pkg/slb/balancer"
+	"github.com/hlts2/round-robin"
 )
 
-type roundrobin struct {
+type roundrobinHandler struct {
+	rr      roundrobin.RoundRobin
+	proxier balancer.Proxier
 	balancer.Handler
 }
 
-func (h *roundrobin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// TODO: not yet implemented
+func (h *roundrobinHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	h.proxier.Proxy(h.rr.Next(), w, req)
 }
 
-func (h *roundrobin) isBalaner() {}
+func (h *roundrobinHandler) isBalaner() {}
 
 // New returns balancer.Handler implementation(*roundrobin).
-func New(urls []url.URL, proxier balancer.Proxier) balancer.Handler {
-	return new(roundrobin)
+func New(urls []*url.URL, proxier balancer.Proxier) balancer.Handler {
+	rr, _ := roundrobin.New(urls)
+
+	return &roundrobinHandler{
+		rr:      rr,
+		proxier: proxier,
+	}
 }
