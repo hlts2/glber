c8b62399db9fe742271abe048e64bdd2289e46f1
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-14 19:12:11 +0900

    Deleted unnecessary log

--- pkg/slb/slb.go
+++ pkg/slb/slb.go
@@ -3,7 +3,6 @@ package slb
 import (
 	"context"
 	"crypto/tls"
-	"fmt"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -58,8 +57,6 @@ func CreateSLB(cfg *Config, ops ...Option) (Server, error) {
 	sbl.Server = &http.Server{
 		Handler: sbl.HandlerDirector(cfg.BackendServerConfigs.getURLs(), sbl),
 	}
-
-	fmt.Println(cfg.BackendServerConfigs.getURLs())
 
 	return sbl, nil
 }
